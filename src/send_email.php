c7a4c9bb6bc42564ea89cbf6cf82e405674c91ab
martinezlizux <63366440+martinezlizux@users.noreply.github.com>
2025-04-28 11:20:56 -0700

    envio de correo

---
+++ src/send_email.php
@@ -0,0 +1,26 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $to = "contact.alexahousecleaning@gmail.com";
+    $subject = "New Contact Form Submission - Alexa House Cleaning";
+
+    $name = strip_tags(trim($_POST["Name"]));
+    $lastName = strip_tags(trim($_POST["Last_name"]));
+    $email = filter_var(trim($_POST["Email"]), FILTER_SANITIZE_EMAIL);
+    $message = trim($_POST["Message"]);
+
+    $email_content = "Name: $name\n";
+    $email_content .= "Last Name: $lastName\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$message\n";
+
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $email_content, $headers)) {
+        echo "success";
+    } else {
+        echo "error";
+    }
+} else {
+    echo "invalid";
+}
+?>
